37b57780c2a24225625e88e966565cc77c2460e8
odnokoz <odnokozova@bk.ru>
2018-12-06 16:21:51 +0300

    first commit

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-document.querySelector('.question__text').style.display = 'none';
+document.querySelector('.question-box__item--hint').style.opacity = '0';
 
-const corrAns = document.querySelector('.btn__answer--correct');
+const corrAns = document.querySelector('.js-answer-correct');
 corrAns.addEventListener('click', function() {
 	corrAns.style.backgroundColor = '#e5975d';
 	corrAns.style.color = 'white';
 });
 
-const uncorrAns = document.querySelectorAll('.btn__answer--uncorrect');
+const uncorrAns = document.querySelectorAll('.js-answer-uncorrect');
 
 const uncorrAnsArr = Array.from(uncorrAns);
 
 for (var i = 0; i < uncorrAnsArr.length; i++) {
 	uncorrAnsArr[i].addEventListener('click', function() {
-		document.querySelector('.question__text').style.display = 'block';
+		document.querySelector('.question-box__item--hint').style.opacity = '100';
 		corrAns.style.backgroundColor = '#f3f5f8';
 		corrAns.style.color = '#362d29';
 	});	
